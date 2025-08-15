e5b67a2bfe112a41082d28b71cf6817f905b1326
Viv <vivax3794@protonmail.com>
2025-08-15 14:04:18 +0200

    stuff

--- visual/home.nix
+++ visual/home.nix
@@ -83,4 +83,12 @@
       onlySpeakers = true;
     };
   };
+
+  home.activation = {
+    # https://github.com/philj56/tofi/issues/115#issuecomment-1701748297
+    regenerateTofiCache = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+      tofi_cache=${config.xdg.cacheHome}/tofi-drun
+      [[ -f "$tofi_cache" ]] && rm "$tofi_cache"
+    '';
+  };
 }
